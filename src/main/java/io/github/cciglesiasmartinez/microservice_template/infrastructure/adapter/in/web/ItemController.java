639b5e05804e268deebdc59acade437bc46422ac
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-10-16 05:08:28 +0200

    Adds create and get item routes plus OpenAPI documentation.

--- src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/adapter/in/web/ItemController.java
+++ src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/adapter/in/web/ItemController.java
@@ -1,24 +1,30 @@
 package io.github.cciglesiasmartinez.microservice_template.infrastructure.adapter.in.web;
 
 import io.github.cciglesiasmartinez.microservice_template.application.port.in.ItemUseCase;
-import io.github.cciglesiasmartinez.microservice_template.infrastructure.adapter.in.web.dto.requests.GetItemRequest;
+import io.github.cciglesiasmartinez.microservice_template.infrastructure.adapter.in.web.dto.requests.CreateItemRequest;
+import io.github.cciglesiasmartinez.microservice_template.infrastructure.adapter.in.web.dto.responses.CreateItemResponse;
 import io.github.cciglesiasmartinez.microservice_template.infrastructure.adapter.in.web.dto.responses.Envelope;
 import io.github.cciglesiasmartinez.microservice_template.infrastructure.adapter.in.web.dto.responses.GetItemResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
- *
+ * Este será el controlador REST. Aquí irán las anotaciones de OpenAPI/Swagger además de la llamada a la interfaz
+ * {@link ItemUseCase} que residirá en la capa de aplicación.
+ * <p>
+ * Es conveniente notar que esta capa no interactúa directamente con otras partes de la infraestructura, como los
+ * repositorios MySQL; eso se llevará a cabo en la capa de aplicación (o sea, en los use cases).
  */
 @AllArgsConstructor
 @RestController
-@RequestMapping("/item")
+@RequestMapping("/items")
+// La etiqueta @Tag será empleada para anotar el nombre del servicio y dar una pequeña descripción para Swagger.
 @Tag(
         name = "Item service.",
         description = "Endpoints related to Item entity. CRUD operations."
@@ -27,10 +33,48 @@ public class ItemController {
 
     private ItemUseCase itemUseCase;
 
+    /**
+     * Ruta del API REST.
+     * <p>
+     * Debe ir anotada con la correspondiente información para Swagger (anotaciones {@code @Operation} y
+     * {@code ApiResponses}).
+     *
+     * @param id el ID del item a consultar.
+     * @return un objeto de tipo {@link ResponseEntity} con el DTO correspondiente.
+     */
+    @Operation(
+            summary = "Retrieves an item.",
+            description = "Retrieves the information available for an item given its id."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item retrieved successfully.")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Envelope<GetItemResponse>> getItem(@Valid @RequestBody GetItemRequest request) {
-        Envelope<GetItemResponse> response = itemUseCase.getItem(request);
-        return null;
+    public ResponseEntity<Envelope<GetItemResponse>> getItem(@PathVariable String id) {
+        Envelope<GetItemResponse> response = itemUseCase.getItem(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    /**
+     * Ruta del API REST.
+     * <p>
+     * Debe ir anotada con la correspondiente información para Swagger (anotaciones {@code @Operation} y
+     * {@code ApiResponses}).
+     *
+     * @param request el DTO para el caso de uso correspondiente ({@link CreateItemRequest}).
+     * @return un objeto de tipo {@link ResponseEntity} con el DTO correspondiente.
+     */
+    @Operation(
+            summary = "Creates a new item.",
+            description = "Creates a new item given its name."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item created successfully.")
+    })
+    @PostMapping("/")
+    public ResponseEntity<Envelope<CreateItemResponse>> createItem(@RequestBody CreateItemRequest request) {
+        Envelope<CreateItemResponse> response = itemUseCase.createItem(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
