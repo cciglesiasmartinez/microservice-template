1cc040ff5efbd63e4f57af1f7441f5345cead089
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-10-20 08:24:55 +0200

    Changes DomainEvent type interface to abstract class.

--- src/main/java/io/github/cciglesiasmartinez/microservice_template/domain/event/DomainEvent.java
+++ src/main/java/io/github/cciglesiasmartinez/microservice_template/domain/event/DomainEvent.java
@@ -1,8 +1,21 @@
 package io.github.cciglesiasmartinez.microservice_template.domain.event;
 
+import lombok.Getter;
+import org.slf4j.MDC;
+
 /**
  * Esto queda como un "placeholder" para los eventos de dominio. Será interesante debatir en el futuro qué puede
  * incluirse en esta interfaz con el objeto de enriquecer los eventos de alguna manera estándar.
  */
-public interface DomainEvent {
+@Getter
+public abstract class DomainEvent {
+
+    private String requestId;
+    private String eventType;
+
+    public DomainEvent(String eventType) {
+        this.requestId = MDC.get("requestId");
+        this.eventType = eventType;
+    }
+
 }
