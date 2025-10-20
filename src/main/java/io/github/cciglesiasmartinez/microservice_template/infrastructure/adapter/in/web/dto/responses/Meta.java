e486b9b56b3eef9c0f3df39b576e6b8bae8b79b9
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-10-21 01:43:09 +0200

    Adds comments and annotations to Meta class.

--- src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/adapter/in/web/dto/responses/Meta.java
+++ src/main/java/io/github/cciglesiasmartinez/microservice_template/infrastructure/adapter/in/web/dto/responses/Meta.java
@@ -4,13 +4,23 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 import org.slf4j.MDC;
 
 import java.time.LocalDateTime;
 
+/**
+ * Esto es parte de un DTO de respuesta estándar. Un DTO de salida {@link Envelope} siempre tendrá un apartado "data"
+ * que coincidirá con un DTO de respuesta y luego un apartado "meta" que contendrá esta clase.
+ * <p>
+ * Mayormente esta clase contendrá
+ */
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Standard meta-data for all API responses.")
+@Getter
+@Setter
 public class Meta {
 
     @Schema(
