e18472d67e8284dba878d6d23f3dc62809028b6e
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-10-21 01:44:20 +0200

    Adds serialization to ItemCreatedEvent plus getter.

--- src/main/java/io/github/cciglesiasmartinez/microservice_template/domain/event/ItemCreatedEvent.java
+++ src/main/java/io/github/cciglesiasmartinez/microservice_template/domain/event/ItemCreatedEvent.java
@@ -1,7 +1,11 @@
 package io.github.cciglesiasmartinez.microservice_template.domain.event;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.github.cciglesiasmartinez.microservice_template.domain.model.valueobjects.ItemId;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -10,14 +14,17 @@ import java.time.LocalDateTime;
  * <p>
  * Será interesante discutir si un evento a nivel interno deberá coincidir en forma y tipo con el evento externo.
  * Es decir, ¿debe un evento propagado por Kafka tener el mismo formato y datos que el evento propagado internamente
- * en la aplicación? Esto será un buen punto de consideración futura. Por el momento, diremos que son entidades
- * separadas.
+ * en la aplicación? Esto será un buen punto de consideración futura. Por el momento lo haremos así, ya habrá ocasión
+ * de complicar más las cosas.
  * <p>
  * En este caso particular recogemos el itemId del Item, pues será el único dato que propagaremos. No obstante, en
  * otros casos podrá ser interesante propagar más datos.
  * <p>
  * Además, podría ser interesante también propagar otros eventos, como cuando un Item se actualice o se elimine.
  */
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Setter
 @Getter
 public class ItemCreatedEvent extends DomainEvent {
 
